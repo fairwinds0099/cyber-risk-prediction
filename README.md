6b963ac2e0b26032962c684480b0f434d9f66c3c
selametle <selametle@mail.com>
2020-11-25 21:44:53 -0600

    refactoring methods

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ocean_analysis
 OCEAN test data set
+
+## data set preparation
+add test data path `results_with_scenarios.csv` to DataPrepActions
--- main.py
+++ main.py
@@ -3,48 +3,51 @@ from imblearn.over_sampling import RandomOverSampler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score,roc_curve,recall_score,roc_auc_score,accuracy_score,precision_score,classification_report,confusion_matrix
-
 from DataAnalyzeActions import DataAnalyzeActions
 from DataPrepActions import DataPrepActions
 
-dataPrepActions = DataPrepActions();
-dataAnalyzeActions = DataAnalyzeActions();
+dataPrepActions = DataPrepActions()
+dataAnalyzeActions = DataAnalyzeActions()
+randomOverSampler = RandomOverSampler(random_state=0)
+randomForestClassifier = RandomForestClassifier(random_state=0)
 
 if __name__ == '__main__':
 
     X, y = dataPrepActions.fetchAndCleanDataframe()
-    ros = RandomOverSampler(random_state=0)
-    X_ros, y_ros = ros.fit_sample(X, y)
 
-    rfc = RandomForestClassifier(random_state=0)
-    X_train, Xtest, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, stratify=y)
-    X_train_val, X_val, y_train_val, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1, stratify=y_train)
-    rfc.fit(X_train_val, y_train_val)
-    predictions=rfc.predict(X_val)
-    print('Classification report:]\n', classification_report(y_val, predictions))
-    #conf_mat = confusion_matrix(y)
-    #print('Confusion matrix:\n', conf_mat)
+    # splitting raw dataset to three: train validation test split and printing sizes
+    xTrain, xTest, yTrain, yTest = train_test_split(X, y, test_size=0.2, random_state=1, stratify=y)
+    xTrainVldtn, xVldtn, yTrainVldtn, yVldtn = train_test_split(xTrain, yTrain, test_size=0.25, random_state=1, stratify=yTrain)
+    print('=================  data size =========================')
+    print('x (raw) size:', len(X), 'y (raw) size:', len(y))
+    print('xTrain size:', len(xTrain), 'yTrain size:', len(yTrain))
+    print('xTest size:', len(xTest), 'yTest size:', len(yTest))
+    print('xTrainVldtn size:', len(xTrainVldtn), 'yTrainVldtnSize:', len(yTrainVldtn))
 
-    rf_ros = RandomForestClassifier()
-    X_train_val_ros, y_train_val_ros = ros.fit_sample(X_train_val, y_train_val)
-    rf_ros.fit(X_train_val_ros, y_train_val_ros)
-    prediction = rf_ros.predict(X_val)
-    print('Classifcation report:\n', classification_report(y_val, prediction))
-    conf_mat = confusion_matrix(y_val, prediction)
-    print('Confusion matrix:\n', conf_mat)
-    prob = rf_ros.predict_proba(X_val)[:, 1]
-    fpr, tpr, thresholds  = roc_curve(y_val, prob) #Get the ROC Curve
-    roc_auc_score(y_val, prob)
-    dataAnalyzeActions.scores_val(rf_ros, 'Random Over Sampling')
-    dataAnalyzeActions.precision_recall_threshold(fpr, tpr, thresholds, t=0.2424)
-    plt.figure(figsize=(8, 5))
-    # Plot ROC curve
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.plot(fpr, tpr)
-    plt.xlabel('False Positive Rate = 1 - Specificity Score')
-    plt.ylabel('True Positive Rate  = Recall Score')
-    plt.title('ROC Curve for Random Under Sampling')
-    plt.show()
+    randomForestClassifier.fit(xTrainVldtn, yTrainVldtn)
+    predictions = randomForestClassifier.predict(xVldtn)
+    print('Classification report:]\n', classification_report(yVldtn, predictions))
+    print('Confusion matrix:\n', confusion_matrix(yVldtn, predictions))
+
+    xTrainVldtnOverSampled, yTrainValidationOverSampled = randomOverSampler.fit_sample(xTrainVldtn, yTrainVldtn)
+    randomForestClassifier.fit(xTrainVldtnOverSampled, yTrainValidationOverSampled)
+    predictions = randomForestClassifier.predict(xVldtn)
+    print('Classification report:]\n', classification_report(yVldtn, predictions))
+    print('Confusion matrix:\n', confusion_matrix(yVldtn, predictions))
+
+# prob = rf_ros.predict_proba(X_val)[:, 1]
+    # fpr, tpr, thresholds  = roc_curve(y_val, prob) #Get the ROC Curve
+    # roc_auc_score(y_val, prob)
+    # dataAnalyzeActions.scores_val(rf_ros, 'Random Over Sampling')
+    # dataAnalyzeActions.precision_recall_threshold(fpr, tpr, thresholds, t=0.2424)
+    # plt.figure(figsize=(8, 5))
+    # # Plot ROC curve
+    # plt.plot([0, 1], [0, 1], 'k--')
+    # plt.plot(fpr, tpr)
+    # plt.xlabel('False Positive Rate = 1 - Specificity Score')
+    # plt.ylabel('True Positive Rate  = Recall Score')
+    # plt.title('ROC Curve for Random Under Sampling')
+    # plt.show()
 
 
 
