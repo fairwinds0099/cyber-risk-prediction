c1b1b73a4d7c94a24a80fcfc92f4c522ed994d9a
selametle <selametle@mail.com>
2020-11-27 14:24:17 -0600

    comments added

--- DataPrepActions.py
+++ DataPrepActions.py
@@ -12,13 +12,13 @@ class DataPrepActions():
         """
 
         df = pd.read_csv('/Users/apple4u/Desktop/goksel tez/results_with_scenarios.csv')
-        df.insider_label.fillna(0, inplace=True)
+        df.insider_label.fillna(0, inplace=True) # replaces null fields with 0
         df = df.drop(columns=['employee_name', 'user_id'])
         df['label'] = df['insider_label'].astype('int64')
         df.drop(columns='insider_label', inplace=True)
         df.drop(columns='scenario', inplace=True)
         df.drop(columns='role', inplace=True)
         print(df.head())
-        X = df.iloc[:, :5].values
+        X = df.iloc[:, :5].values #fetch all records first 5 columns
         y = df.label.values
         return X, y
