e512d70d8fc59fa903365a1f0d1d76cb52734005
selametle <selametle@mail.com>
2020-11-29 22:39:21 -0600

    updated with session on Nov 29

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import matplotlib.pyplot as plt
-from imblearn.over_sampling import RandomOverSampler
+from imblearn.over_sampling import ADASYN, RandomOverSampler, SMOTE
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score,roc_curve,recall_score,roc_auc_score,accuracy_score,precision_score,classification_report,confusion_matrix
 from DataAnalyzeActions import DataAnalyzeActions
 from DataPrepActions import DataPrepActions
+from imblearn.ensemble import EasyEnsembleClassifier
 
 dataPrepActions = DataPrepActions()
 dataAnalyzeActions = DataAnalyzeActions()
 randomOverSampler = RandomOverSampler(random_state=0)
 randomForestClassifier = RandomForestClassifier(random_state=0)
+smote = SMOTE()
+ada = ADASYN()
+easy = EasyEnsembleClassifier()
+
 
 if __name__ == '__main__':
 
@@ -32,17 +37,53 @@ if __name__ == '__main__':
     print('Confusion matrix:\n', confusion_matrix(yVldtn, predictions))
     del randomForestClassifier
 
-    # oversampling insiders
+    # data imbalance mitigation: oversampling insiders
     randomForestClassifier = RandomForestClassifier(random_state=0)
     xTrainOverSampled, yTrainOverSampled = randomOverSampler.fit_sample(xTrain, yTrain)
-    print('xTrain size:', len(xTrainOverSampled), 'yTrainSize:', len(yTrainOverSampled))
+    print('xTrain size with Random Over Sampling:', len(xTrainOverSampled),
+          'yTrainSize with Random Over Sampling:', len(yTrainOverSampled))
     randomForestClassifier.fit(xTrainOverSampled, yTrainOverSampled)
     predictions = randomForestClassifier.predict(xVldtn)
     print('Classification report:]\n', classification_report(yVldtn, predictions))
     print('Confusion matrix:\n', confusion_matrix(yVldtn, predictions))
+    del randomForestClassifier
+    del predictions
 
-    probabilities = randomForestClassifier.predict_proba(X)[:, 1]
-    print(len(probabilities))
+    # data imbalance mitigation: SMOTE
+    randomForestClassifier = RandomForestClassifier(random_state=0)
+    xTrainSmote, yTrainSmote = smote.fit_sample(xTrain, yTrain)
+    print('xTrain size with SMOTE: ', len(xTrainSmote),
+          'yTrainSize with SMOTE: ', len(yTrainSmote))
+    randomForestClassifier.fit(xTrainSmote, yTrainSmote)
+    predictions = randomForestClassifier.predict(xVldtn)
+    print('Classification report:\n', classification_report(yVldtn, predictions))
+    print('Confusion matrix:\n', confusion_matrix(yVldtn, predictions))
+    del randomForestClassifier
+
+    # data imbalance mitigation: ADASYN
+    randomForestClassifier = RandomForestClassifier(random_state=0)
+    xTrainAdasyn, yTrainAdasyn = ada.fit_sample(xTrain, yTrain)
+    print('xTrain size with ADASYN: ', len(xTrainAdasyn),
+          'yTrainSize with ADASYN: ', len(yTrainAdasyn))
+    randomForestClassifier.fit(xTrainAdasyn, yTrainAdasyn)
+    predictions = randomForestClassifier.predict(xVldtn)
+    print('Classification report:]\n', classification_report(yVldtn, predictions))
+    print('Confusion matrix:\n', confusion_matrix(yVldtn, predictions))
+    del randomForestClassifier
+
+    # data imbalance mitigation: ADASYN
+    randomForestClassifier = RandomForestClassifier(random_state=0)
+    xTrainEasy, yTrainEasy = easy.fit(xTrain, yTrain)
+    print('xTrain size with EASY: ', len(xTrainEasy),
+          'yTrainSize with EASY: ', len(yTrainEasy))
+    randomForestClassifier.fit(xTrainEasy, yTrainEasy)
+    predictions = randomForestClassifier.predict(xVldtn)
+    print('Classification report:]\n', classification_report(yVldtn, predictions))
+    print('Confusion matrix:\n', confusion_matrix(yVldtn, predictions))
+    del randomForestClassifier
+
+    # probabilities = randomForestClassifier.predict_proba(X)[:, 1]
+    # print(len(probabilities))
 
     # falsePositiveRates, truePositiveRates, thresholds  = roc_curve(yVldtn, probabilities) #Get the ROC Curve
     # roc_auc_score(yVldtn, probabilities)
